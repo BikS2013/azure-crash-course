d5f04a7364cbf9e19cbb334550b1b6b5dfbc0ed5
akyrgiazos <37440235+akyrgiazos@users.noreply.github.com>
2025-03-17 14:25:17 +0200

    add search manager window

--- search_manager_window.py
+++ search_manager_window.py
@@ -10,95 +10,10 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import AzureChatOpenAI
 
 from logger import log, INFO, ERROR, WARNING
-from search_endpoint import SearchEndPoint
-from search_index import SearchIndex
-from search_schema import schema
-
-from config import (
-    AZURE_SEARCH_ENDPOINT,
-    AZURE_SEARCH_KEY,
-    AZURE_SEARCH_INDEX_NAME,
-    AZURE_SEARCH_API_VERSION,
-    AZURE_OPENAI_ENDPOINT,
-    AZURE_OPENAI_CHAT_MODEL,
-    AZURE_OPENAI_API_VERSION,
-    AZURE_OPENAI_KEY,
-    DOMAINS,
-)
-
-class SearchManager :
-    search_endpoint: SearchEndPoint
-    chat_model: BaseChatModel
-    domains: List[str]
-    def __init__(self, search_endpoint: SearchEndPoint, chat_model: BaseChatModel):
-        self.chat_model = chat_model
-        self.search_endpoint = search_endpoint
-        self.domains = DOMAINS
+from tenacity import retry, stop_after_attempt, wait_random_exponential
 
 
-    def get_filter_conditions(self, unique_chunks: List[str], window: int = 3) -> str:
-        """
-        Generate filter conditions for unique titles
-        """
-        filter_conditions = []
-        for chunk in unique_chunks:
-            # Replace single quotes with double single quotes to escape them in OData
-            escaped_chunk = chunk.replace("'", "''")
-            for i in range(-window, window+1):
-                filter_conditions.append(f"chunk_id eq '{str(int(escaped_chunk)+i)}'")
-        
-        return " or ".join(filter_conditions)
-    
-    def category_prompt(self) -> PromptTemplate:
-        # Create prompt template
-        from prompts import category_prompt_template
-        return PromptTemplate(template=category_prompt_template, input_variables=['question'])
-    
-    def category_chain(self, question: str) -> str:
-        """
-        Get the category of the question
-        """
-        chain = self.category_prompt() | self.chat_model | StrOutputParser()
-        result = chain.invoke({"question": question})
-        return result 
-
-    def search(self, query: str, top_k: int = 20, type_filter: str = "") -> List[Dict[str, Any]]:
-        """
-        Search documents using hybrid similarity search, pre filtering and then retrieve all the content of the unique documents
-        """
-        try:
-            type_filter = self.category_chain(query)
-            if type_filter in self.domains:
-                # Perform similarity search
-                results = self.search_endpoint.hybrid_search(query, top_k=top_k, filters=f"type eq '{type_filter}'")
-            else:
-                results = self.search_endpoint.hybrid_search(query, top_k=top_k)
-
-            # Get unique chunks from results
-            unique_chunks = list(set([res[0].metadata.get('chunk_id', '') for res in results]))            
-            filter_expression = self.get_filter_conditions(unique_chunks)
-            
-            # Execute the search with the filter to get all the content of the documents
-            results_all = self.search_endpoint.hybrid_search(query, top_k=100, filters=filter_expression)
-
-            # Format results
-            search_results = []
-            for doc, score in results_all:
-                result = {
-                    'content': doc.page_content,
-                    'source': doc.metadata.get('title', ''),
-                    'score': score,
-                    **{k: v for k, v in doc.metadata.items() if k != 'source'}
-                }
-                search_results.append(result)
-            # log(INFO, f"Search completed successfully - Found {len(search_results)} results")
-            return search_results
-
-        except Exception as e:
-            log(ERROR, f"Search error: {str(e)}")
-            return []
-    
-def initialize_search_manager() -> tuple[SearchManager, BaseChatModel]:
+def search_manager() -> List[Dict]:
     """Initialize Azure Search with OpenAI embeddings"""
     try:
 
@@ -110,9 +25,31 @@ def initialize_search_manager() -> tuple[SearchManager, BaseChatModel]:
             AZURE_RESOURCE_GROUP,
             AZURE_SEARCH_SERVICE_NAME,
             AZURE_INDEX_NAME,
-            AZURE_INDEX_SEMANTIC_CONFIGURATION
+            AZURE_INDEX_SEMANTIC_CONFIGURATION,
+            AZURE_OPENAI_ENDPOINT,
+            AZURE_OPENAI_EMBEDDING_MODEL,
+            AZURE_OPENAI_API_VERSION,
+            AZURE_OPENAI_KEY,
         )
+        from openai import AzureOpenAI
+        def get_embedding_client( api_key: str, api_base: str, api_version: str) : 
+            client = AzureOpenAI(
+                api_key=api_key,  # Set this as an environment variable
+                api_version=api_version,           # Using a recent API version
+                azure_endpoint=api_base,  # e.g. "https://your-resource.openai.azure.com/"
+            )
+            return client
 
+        @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
+        def generate_embeddings(text: str, openai_client, model: str = "text-embedding-3-small" ) -> List[float]:
+            try : 
+                model_deployed = model
+                response = openai_client.embeddings.create( input=[text],model="text-embedding-3-small" )
+                return response.data[0].embedding
+            except Exception as e:
+                print(f"Error: {str(e)}")
+                raise e 
+   
         from wrapper import Identity
 
         identity = Identity(AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET)
@@ -128,66 +65,22 @@ def initialize_search_manager() -> tuple[SearchManager, BaseChatModel]:
         result = index.perform_search(query_text="Ποιά είναι τα καταναλωτικά δανειακά προϊόντα?", highlight_fields="chunk", top=5)
         list = [item for item in result]
         
-        exit(); 
         semantic_config_name = AZURE_INDEX_SEMANTIC_CONFIGURATION
         query="Ποιά είναι τα καταναλωτικά δανειακά προϊόντα?"
+        openai_client = get_embedding_client(AZURE_OPENAI_KEY, api_base=AZURE_OPENAI_ENDPOINT, api_version=AZURE_OPENAI_API_VERSION )
+        query_embedding = generate_embeddings(query, openai_client, model=AZURE_OPENAI_EMBEDDING_MODEL)
         if index is not None:
-            result = index.perform_hybrid_search(query_text=query, 
-                                            #query_vector=query_embedding, 
-                                            vector_fields="text_vector", 
-                                            use_semantic_search=True,
-                                            semantic_config_name=semantic_config_name)
-            
-            
-        
+            result = index.search_with_context_window(
+                            query_text=query,
+                            query_vector=query_embedding,
+                            vector_fields="text_vector",
+                            use_semantic_search=True,
+                            semantic_config_name=semantic_config_name,
+                            window_size=3,  # 3 chunks before and after
+                            top=5,
+                            )
 
-        exit()
-
-        location = SearchEndPoint(AZURE_SEARCH_ENDPOINT, AZURE_SEARCH_KEY, AZURE_SEARCH_INDEX_NAME, AZURE_SEARCH_API_VERSION, 
-                                  #embeddings
-                                  )
-
-        index = SearchIndex(location, AZURE_SEARCH_INDEX_NAME, schema )
-        
-
-        llm = AzureChatOpenAI(
-            model=AZURE_OPENAI_CHAT_MODEL,
-            temperature=0.0,
-            api_version=AZURE_OPENAI_API_VERSION,
-            azure_endpoint=AZURE_OPENAI_ENDPOINT,
-            api_key=AZURE_OPENAI_KEY,
-        )
-                
-        log(INFO, f"Successfully initialized AzureSearchManager with endpoint: {AZURE_SEARCH_ENDPOINT}")
-        
-        import azure.search.documents.indexes.models as azsdim        
-        def get_index_fields():
-            # Define the index fields
-            fields = [
-                azsdim.SimpleField(name="chunk_id", type=azsdim.SearchFieldDataType.String, key=True),
-                azsdim.SearchableField(name="chunck", type=azsdim.SearchFieldDataType.String, analyzer_name="en.lucene"),
-                azsdim.SearchableField(name="title", type=azsdim.SearchFieldDataType.String, analyzer_name="en.lucene"),
-                azsdim.SimpleField(name="text_vector", type=azsdim.SearchFieldDataType.Collection(azsdim.SearchFieldDataType.Single),
-                                vector_search_dimensions=1536,
-                                vector_search_profile_name="default-profile"),
-                azsdim.SearchableField(name="url", type=azsdim.SearchFieldDataType.String, analyzer_name="en.lucene"),
-                azsdim.SearchableField(name="name", type=azsdim.SearchFieldDataType.String, analyzer_name="en.lucene"),
-            ]
-            return fields
-        
-        index = azsdim.SearchIndex(
-            name=index_name,
-            fields=fields,
-            vector_search=vector_search
-        )        
-
-
-
-        manager = SearchManager(location, index, 
-                                #embed_document, 
-                                llm)
-        return manager
-
+        return result
     except requests.exceptions.RequestException as e:
         error_msg = f"Network error connecting to Azure Search: {str(e)}"
         log(ERROR, error_msg)
@@ -197,4 +90,4 @@ def initialize_search_manager() -> tuple[SearchManager, BaseChatModel]:
         raise
 
 if __name__ == "__main__":
-    manager= initialize_search_manager()
+    result = search_manager()
